aeecb4029cf069984f692d4a74e04aa34324f2cc
palagpatski <palagpatski@gmail.com>
2016-04-13 20:01:43 +0800

    Improve code based on sticky edit actions.

--- wwm_sticky_buttons.js
+++ wwm_sticky_buttons.js
@@ -1,7 +1,7 @@
 (function ($) {
 	Drupal.behaviors.stickyButtons = {
     attach: function (context, settings) {
-      $("#edit-actions").stick_in_parent({parent:"body"});
+      $("#edit-actions").stick_in_parent({parent: 'body'});
     }
   };
 }(jQuery));
