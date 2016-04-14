9a77edd0bca1cb7e993f7507d65f4337dcd7c7c5
palagpatski <palagpatski@gmail.com>
2016-04-14 22:33:37 +0800

    Declare variable

--- wwm_sticky_buttons.js
+++ wwm_sticky_buttons.js
@@ -2,17 +2,20 @@
 	Drupal.behaviors.stickyButtons = {
     attach: function (context, settings) {
 
+        var $this = $('.my-sticky-element');
+
         if (!!$this.offset()) {  // make sure ".my-sticky-element" element exists
 
             $(window).on("load resize",function() {           // fire the script on load and resize
               $this.css('position','static');  // this is to reset the position of the element whenever the page is updated with AJAX.
-              $this.css('width', '100%');      // reset button width
 
               var stickyTop = $this.offset().top;
               var windowHeight = $(window).height();
               var buttonWidth = $this.width();
               var windowTop = $(window).scrollTop(); // returns number  
               var currentPosition = windowTop + windowHeight;
+
+              $this.css('width', buttonWidth);      // reset button width
 
               if (stickyTop > currentPosition) {
                 $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: buttonWidth });
