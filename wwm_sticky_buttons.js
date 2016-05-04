e7cf42fca51cc633d13eda8b83eae50c24b87ac4
palagpatski <palagpatski@gmail.com>
2016-05-04 10:52:14 +0800

    Improve code

--- wwm_sticky_buttons.js
+++ wwm_sticky_buttons.js
@@ -2,47 +2,47 @@
     Drupal.behaviors.stickyButtons = {
     attach: function (context, settings) {
 
+        var stickyTop;
+        var windowHeight;
+        var buttonWidth;
+        var buttonHeight;
+        var windowTop;
+        var currentPosition;
+        var $this = $('.my-sticky-element');
+
+        function reset() {
+            $this.css({position: 'static', width: 'initial'});
+            
+            stickyTop = $this.offset().top;       // tells how far our target element is from the top of the page
+            windowHeight = $(window).height();    // measures the window height
+            buttonWidth = $this.width(); // gets the width of our button
+            buttonHeight = $this.height();        // gets the height of our button
+
+            console.log(buttonWidth);
+
+            $this.width( buttonWidth );
+        }
+
+        function scrollSticky() {    
+            windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
+            currentPosition = windowTop + windowHeight;    // tells how far our target element is from where our screen is currently 
+
+            // console.log(stickyTop);
+            // console.log(currentPosition - buttonHeight);
+            // console.log(stickyTop - (currentPosition - (buttonHeight)));
+
+            if (stickyTop > (currentPosition - (buttonHeight))) {    // if target element goes below the screen
+              $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: buttonWidth, 'box-sizing': 'content-box' });   // stick it to the bottom
+            }
+            else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
+              $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: buttonWidth, 'box-sizing': 'content-box' });   //stick it at the top
+            }
+            else {
+              $this.css({ position: 'static', width: buttonWidth, 'box-sizing': 'content-box' });
+            }
+        }
+
         if (($(window).width() > 480) && (!!$('.my-sticky-element').offset())) {
-            var stickyTop;
-            var windowHeight;
-            var buttonWidth;
-            var buttonHeight;
-            var windowTop;
-            var currentPosition;
-            var $this = $('.my-sticky-element');
-    
-            function reset() {
-                $this.css({position: 'static', width: 'initial'});
-                
-                stickyTop = $this.offset().top;       // tells how far our target element is from the top of the page
-                windowHeight = $(window).height();    // measures the window height
-                buttonWidth = $this.width(); // gets the width of our button
-                buttonHeight = $this.height();        // gets the height of our button
-
-                console.log(buttonWidth);
-
-                $this.width( buttonWidth );
-            }
-
-            function scrollSticky() {    
-                windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
-                currentPosition = windowTop + windowHeight;    // tells how far our target element is from where our screen is currently 
-    
-                // console.log(stickyTop);
-                // console.log(currentPosition - buttonHeight);
-                // console.log(stickyTop - (currentPosition - (buttonHeight)));
-    
-                if (stickyTop > (currentPosition - (buttonHeight))) {    // if target element goes below the screen
-                  $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: buttonWidth, 'box-sizing': 'content-box' });   // stick it to the bottom
-                }
-                else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
-                  $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: buttonWidth, 'box-sizing': 'content-box' });   //stick it at the top
-                }
-                else {
-                  $this.css({ position: 'static', width: buttonWidth, 'box-sizing': 'content-box' });
-                }
-            }
-
             // $(window).off("load resize");
             $(window).on("load resize", reset);
     
@@ -50,8 +50,8 @@
             $(window).on("load resize scroll", scrollSticky); 
 
             $(document).load(function(){
-                $(document).bind('DOMNodeInserted', reset); // reset when new elements are inserted
-            })
+                $(this).bind('DOMNodeInserted', reset); // reset when new elements are inserted
+            });
         }
     }
   };
