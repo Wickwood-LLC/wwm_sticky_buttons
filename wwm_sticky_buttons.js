7ba3820a0d8e5959411a763cc5aafac273adcd11
palagpatski <palagpatski@gmail.com>
2016-10-18 14:55:06 +0800

    Get the sticky buttons firing on load

--- wwm_sticky_buttons.js
+++ wwm_sticky_buttons.js
@@ -10,13 +10,6 @@
         var windowTop;
         var currentPosition;
 
-        stickyTop = $this.offset().top;       // tells how far our target element is from the top of the page
-        windowHeight = $(window).height();    // measures the window height
-        buttonWidth = $this.width(); // gets the width of our button
-        buttonHeight = $this.height();        // gets the height of our button
-        $this.width( buttonWidth );
-        // $this.css({position: 'static', top: 'initial', bottom: 'initial', width: buttonWidth, 'box-sizing': 'content-box'});
-
         function reset() {
             // if (detectIEregexp.test(navigator.userAgent)){ //if some form of IE
             //     buttonWidth = $this.width('inherit');
@@ -30,7 +23,20 @@
             // console.log(buttonWidth);
             $this.width( buttonWidth );
             $this.css({position: 'static', top: 'initial', bottom: 'initial', width: buttonWidth, 'box-sizing': 'content-box'});
-            // $this.css({position: 'static', width: buttonWidth});
+
+
+            windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
+            currentPosition = windowTop + windowHeight;    // tells how far our target element is from where our screen is currently 
+
+            if (stickyTop > (currentPosition - (buttonHeight))) {    // if target element goes below the screen
+              $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: buttonWidth, 'box-sizing': 'content-box' });   // stick it to the bottom
+            }
+            else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
+              $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: buttonWidth, 'box-sizing': 'content-box' });   //stick it at the top
+            }
+            else {
+              $this.css({ position: 'static', top: 'initial', bottom: 'initial', width: buttonWidth, 'box-sizing': 'content-box' });
+            }
         }
 
         function scrollSticky() {    
@@ -52,10 +58,8 @@
             }
         }
 
-        // reset();
-
         if (($(window).width() > 480) && (!!$('.my-sticky-element').offset()) && (!$("body").hasClass("page-admin-structure-views"))) {
-            // $(window).on("load resize", reset);
+            $(window).on("load resize", reset);
             $(window).on("load resize scroll", scrollSticky);
             // $(document).load(function(){
                 // $(document).bind('DOMNodeInserted', reset); // reset when new elements are inserted
