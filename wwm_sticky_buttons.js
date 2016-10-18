94ca958b4309e1fd43d1190b4bc6b05194f38cf8
palagpatski <palagpatski@gmail.com>
2016-10-18 11:31:26 +0800

    Get the sticky buttons firing on load

--- wwm_sticky_buttons.js
+++ wwm_sticky_buttons.js
@@ -48,7 +48,24 @@
 
         if (($(window).width() > 480) && (!!$('.my-sticky-element').offset()) && (!$("body").hasClass("page-admin-structure-views"))) {
             $(window).on("load resize", reset);  
-            $(window).load(scrollSticky);
+            $(window).load(function(){
+                windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
+                currentPosition = windowTop + windowHeight;    // tells how far our target element is from where our screen is currently 
+
+                // console.log(stickyTop);
+                // console.log(currentPosition - buttonHeight);
+                // console.log(stickyTop - (currentPosition - (buttonHeight)));
+
+                if (stickyTop > (currentPosition - (buttonHeight))) {    // if target element goes below the screen
+                  $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: buttonWidth, 'box-sizing': 'content-box' });   // stick it to the bottom
+                }
+                else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
+                  $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: buttonWidth, 'box-sizing': 'content-box' });   //stick it at the top
+                }
+                else {
+                  $this.css({ position: 'static', width: buttonWidth, 'box-sizing': 'content-box' });
+                }
+            });
             $(window).on("resize scroll", scrollSticky);
             // $(document).load(function(){
                 $(document).bind('DOMNodeInserted', reset); // reset when new elements are inserted
