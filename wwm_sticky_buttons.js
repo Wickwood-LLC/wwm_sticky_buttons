e6971155b2f4cb5edaa1e7f10fb1821ffe18a9b4
palagpatski <palagpatski@gmail.com>
2016-10-19 09:40:40 +0800

    Remove unused code

--- wwm_sticky_buttons.js
+++ wwm_sticky_buttons.js
@@ -12,10 +12,6 @@
 
         function scrollSticky() {
             $this.css({position: 'static', width: 'initial'});
-
-            // if (detectIEregexp.test(navigator.userAgent)){ //if some form of IE
-            //     buttonWidth = $this.width('inherit');
-            // }
             
             stickyTop = $this.offset().top;       // tells how far our target element is from the top of the page
             windowHeight = $(window).height();    // measures the window height
@@ -43,25 +39,6 @@
               $this.css({ position: 'static', width: buttonWidth, 'box-sizing': 'content-box' });
             }
         }
-
-        // function scrollSticky() {    
-        //     windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
-        //     currentPosition = windowTop + windowHeight;    // tells how far our target element is from where our screen is currently 
-
-        //     console.log(stickyTop);
-        //     console.log(currentPosition - buttonHeight);
-        //     console.log(stickyTop - (currentPosition - (buttonHeight)));
-
-        //     if (stickyTop > (currentPosition - (buttonHeight))) {    // if target element goes below the screen
-        //       $this.css({ position: 'fixed', top: 'initial', bottom: 0, width: buttonWidth, 'box-sizing': 'content-box' });   // stick it to the bottom
-        //     }
-        //     else if ((stickyTop - windowTop) < 0) {   // if target element goes above the screen
-        //       $this.css({ position: 'fixed', top: '65px', bottom: 'initial', width: buttonWidth, 'box-sizing': 'content-box' });   //stick it at the top
-        //     }
-        //     else {
-        //       $this.css({ position: 'static', width: buttonWidth, 'box-sizing': 'content-box' });
-        //     }
-        // }
 
         if (($(window).width() > 480) && (!!$('.my-sticky-element').offset()) && (!$("body").hasClass("page-admin-structure-views"))) {
             $(window).on("load resize", scrollSticky);    
